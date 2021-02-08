98ae3810ac47963185281b3eb1ead4fd96bfd61a
iiroalhonen <iiro.alhonen@kpsbremen.de>
2021-02-08 10:56:54 +0100

    Initial Commit

---
+++ Shared/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  Shared
+//
+//  Created by Iiro Alhonen on 08.02.21.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
